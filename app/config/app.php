286ce96a4d5c93f19831f778c8e8a38ff9f56c15
Jamie202401 <jhema@outlook.de>
2026-05-01 17:58:23 +0200

    Fix Strukt

---
+++ app/config/app.php
@@ -0,0 +1,12 @@
+<?php
+
+define('APP_NAME',  'RST-Inventar');
+define('APP_VERSION', '1.0.0');
+define('APP_ROOT', dirname(__DIR__));
+
+define('SESSION_LIFETIME', 3600);
+
+define('BARCODE_PREFIX', 'RST-');
+define('BARCODE_TYPE', 'C128');
+
+?>
